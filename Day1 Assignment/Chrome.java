28a6785295987f73dfd50d45fcff9696c56a40d2
subbulakshmi1611 <subbulakshmi16jo@gmail.com>
2024-12-28 12:04:30 +0530

    Add files via upload

---
+++ Day1 Assignment/Chrome.java
@@ -0,0 +1,22 @@
+package Week1.Day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float versionOfType= 91.0f;
+		String developer="Chrome";
+		boolean beta=false;
+		int releaseYear=2000;
+		char shortcutKey='C';
+	
+		System.out.println("Float Value is : " +versionOfType);
+		System.out.println("String Value is : " +developer);
+		System.out.println("Boolean Value is : " +beta);
+		System.out.println("int Value is : " +releaseYear);
+		System.out.println("char Value is : " +shortcutKey);
+		
+		
+
+	}
+
+}
